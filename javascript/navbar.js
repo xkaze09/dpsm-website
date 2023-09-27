7e756bd0a3780ba7dd780fbb75687134f03861b2
Kim-Arcena <kvarcena@up.edu.ph>
2023-09-27 21:04:54 +0800

    add navbar.js

---
+++ javascript/navbar.js
@@ -0,0 +1,18 @@
+// Navbar
+document.addEventListener("DOMContentLoaded", function() {
+    const divisionNavbar = document.querySelector(".division-navbar");
+
+    function toggleNavbarVisibility() {
+        if (window.scrollY > 0) {
+            divisionNavbar.classList.add("show");
+        } else {
+            divisionNavbar.classList.remove("show");
+        }
+    }
+
+    // Initial check on page load
+    toggleNavbarVisibility();
+
+    // Add scroll event listener
+    window.addEventListener("scroll", toggleNavbarVisibility);
+});
