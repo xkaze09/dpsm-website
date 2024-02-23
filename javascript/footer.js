83325025d53ad5fcc36a3534a504700f76f8cb2e
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-02-23 15:24:16 +0800

    Update footer section with contact information and social media icons

---
+++ javascript/footer.js
@@ -0,0 +1,5 @@
+// Telephone Logo Popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
