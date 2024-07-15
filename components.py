da39dff3bbade0ed3a1ddf4f3993dce43e92ab59
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-07-15 15:58:39 +0800

    chore: Added reusable navbars and footer components

--- components.py
+++ components.py
@@ -1,6 +1,5 @@
 import os
 
-
 for root, dirnames, filenames in os.walk("."):
     for filename in filenames:
         if "Bootstrap5-ThemeKit" not in filename and filename.endswith(".html"):
@@ -9,50 +8,72 @@ for root, dirnames, filenames in os.walk("."):
 
             file_content = ""
             should_write = True
+            has_header_script = False
+            has_footer_script = False
+            has_university_navbar = False
+            has_division_navbar = False
+            has_dpsm_footer = False
 
             try:
                 with open(fname, encoding="utf-8") as html:
-                    line = html.readline()
+                    lines = html.readlines()
 
-                    while line:
-                        if "</head>" in line:
+                    # Check existing elements and scripts
+                    for line in lines:
+                        if '<script src="/components/header/university-navbar.js" defer></script>' in line:
+                            has_header_script = True
+                        if '<script src="/components/header/division-navbar.js" defer></script>' in line:
+                            has_header_script = True
+                        if '<script src="/components/footer.js" defer></script>' in line:
+                            has_footer_script = True
+                        if "<university-navbar>" in line:
+                            has_university_navbar = True
+                        if "<division-navbar>" in line:
+                            has_division_navbar = True
+                        if "<dpsm-footer>" in line:
+                            has_dpsm_footer = True
+
+                    # Rebuild the file content with necessary modifications
+                    for line in lines:
+                        # Add the header script if not already present
+                        if not has_header_script and "</head>" in line:
                             file_content += """<script src="/components/header/university-navbar.js" defer></script>
                             <script src="/components/header/division-navbar.js" defer></script>
                             <script src="/components/footer.js" defer></script>\n"""
+                            has_header_script = True
 
-                        # Replaces the navbar section with the component
+                        # Check for university navbar section
                         if "<!-- University Navbar Section -->" in line:
-                            # include the comment first
                             file_content += line
                             should_write = False
                         elif "<!-- End of University Navbar -->" in line:
-                            file_content += "<university-navbar></university-navbar>\n"
+                            if not has_university_navbar:
+                                file_content += "<university-navbar></university-navbar>\n"
                             should_write = True
 
-                        # Replaces the navbar section with the component
+                        # Check for division navbar section
                         if "<!-- Division Navbar Section -->" in line:
-                            # include the comment first
                             file_content += line
                             should_write = False
                         elif "<!-- End of Division Navbar -->" in line:
-                            file_content += "<division-navbar></division-navbar>\n"
+                            if not has_division_navbar:
+                                file_content += "<division-navbar></division-navbar>\n"
                             should_write = True
 
-                        # Replaces the footer section with the component
+                        # Check for footer section
                         if "<!-- Footer -->" in line:
-                            # include the comment first
                             file_content += line
                             should_write = False
                         elif "<!-- End of Footer -->" in line:
-                            file_content += "<dpsm-footer></dpsm-footer>\n"
+                            if not has_dpsm_footer:
+                                file_content += "<dpsm-footer></dpsm-footer>\n"
                             should_write = True
 
-                        # should_write should be true for lines that are not between the navbar or footer
+                        # Append line if not within a replace section
                         if should_write:
                             file_content += line
 
-                        line = html.readline()
-
+                # Write the updated content back to the file
                 with open(fname, "w", encoding="utf-8") as html:
                     html.write(file_content)
 
